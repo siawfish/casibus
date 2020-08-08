e6446c44dcf87a3d4973e7d9526532265a0b36d8
Gerald <mcamanor@gmail.com>
2020-08-08 14:35:45 +0000

    Completed Header component design

---
+++ src/components/featuresComponents/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h4>Home</h4>
+            </div>
+        )
+    }
+}
--- src/components/structureComponents/Midpane.js
+++ src/components/structureComponents/Midpane.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import HomeShareCase from '../featuresComponents/HomeShareCase'
 import Case from '../featuresComponents/Case'
+import Header from '../featuresComponents/Header'
 
 export default class Midpane extends Component {
     render() {
         return (
             <div className="mid">
 
-                <div className="header">
-                    <h4>Home</h4>
-                </div>
+                <Header />
 
                 <HomeShareCase />
 
