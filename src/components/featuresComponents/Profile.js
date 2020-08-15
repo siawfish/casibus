6c48f542d8e56e4623f1e926106a8a01b028ffbd
Gerald <mcamanor@gmail.com>
2020-08-15 22:51:01 +0000

    Completed profile banner design

--- src/components/featuresComponents/Profile.js
+++ src/components/featuresComponents/Profile.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import propic from '../../assets/images/avi.jpg'
 
 export default class Profile extends Component {
     render() {
         return (
-            <div>
-                hi there
+            <div className="profile">
+                <div className="proBanner">
+                    <div className="propicCon">
+                        <img src={propic} alt="propic"/>
+                    </div>
+                </div>
             </div>
         )
     }
