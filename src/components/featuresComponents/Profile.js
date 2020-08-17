1cd9c3d15ea45e0a6dd21e0caaed629a75660611
Gerald <mcamanor@gmail.com>
2020-08-17 23:26:18 +0000

    Completed bioArea design

--- src/components/featuresComponents/Profile.js
+++ src/components/featuresComponents/Profile.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import propic from '../../assets/images/avi.jpg'
+import { MdAddLocation } from 'react-icons/md'
+import { FaHospitalSymbol, FaBirthdayCake } from 'react-icons/fa'
+import { BsFillCalendarFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 export default class Profile extends Component {
     render() {
@@ -8,6 +12,17 @@ export default class Profile extends Component {
                 <div className="proBanner">
                     <div className="propicCon">
                         <img src={propic} alt="propic"/>
+                    </div>
+                    <div className="bioArea">
+                        <button className="btnBorder">Edit profile</button>
+                        <div className="bioCon">
+                            <h5>Dr. Gerald Amanor</h5>
+                            <p className="desc">I love what i do, Being able to save a life is a superpower</p>
+                            <p><small><MdAddLocation /> Accra</small><small><FaHospitalSymbol /> St. Joseph's Hospital</small><small><FaBirthdayCake /> 11 January, 1991</small><small><BsFillCalendarFill /> 01 August, 2020</small></p>
+                            <div className="followersCount">
+                                <Link><span>3,000</span> Followers</Link><Link><span>1,000</span> Following</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
